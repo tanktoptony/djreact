e7484d1855ff684c6cb52862005d6f7784e6c714
tanktoptony <anthony.jarina@gmail.com>
2021-11-10 11:46:08 -0600

    some css, also added room join and create room pages, however, not rendering as of yet

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+
 
 export default class App extends Component {
     constructor(props) {
@@ -7,9 +11,12 @@ export default class App extends Component {
     }
 
     render() {
-        return <h1> Testing React Code </h1>;
+        return (<div>
+            <HomePage/>
+            </div>
+        );
     }
 }
 
 const appDiv = document.getElementById("app");
-render(<App /> , appDiv);
+render(<App name="tony"/> , appDiv);
